a31a664b58bdce785bd897a79dabd63696eb0384
Hajjari Hajjari <hajjari@hajjari.Kuben.it>
2025-09-16 13:53:35 +0200

    Lagt til nåværende tid

--- system.py
+++ system.py
@@ -1,21 +1,23 @@
 import os
 import time
 import platform
-import psutil  # pip install psutil
-
+import psutil
+ 
 def get_uptime():
     boot_time = psutil.boot_time()
     uptime_seconds = time.time() - boot_time
     uptime_str = time.strftime("%H:%M:%S", time.gmtime(uptime_seconds))
     return uptime_str
-
-
+ 
 def main():
     print("=== Systeminfo ===")
     print(f"OS: {platform.system()} {platform.release()}")
     print(f"Maskin: {platform.machine()}")
     print(f"Prosessor: {platform.processor()}")
     print(f"Uptime: {get_uptime()}")
-
+    print(f"Nåværende tid: {time.strftime('%Y-%m-%d %H:%M:%S')}")
+ 
 if __name__ == "__main__":
     main()
+
+
